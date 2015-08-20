9d93a173f29c3c7e4637e3d96321b49d69dc3792
Ribrumfield <ribrumfield@NN123-HA-24.HS.sd203.org>
2015-08-20 14:11:29 -0500

    tested out turtle functions

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,7 +6,7 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
     }
