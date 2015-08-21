0fb49fa6177e79ac84d67a1df4014f1e44f65f50
Ribrumfield <ribrumfield@NN123-HA-24.HS.sd203.org>
2015-08-21 14:13:36 -0500

    worked with new methods on the turtles

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -9,5 +9,19 @@ public class TurtleWorld
         turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(80);
+        turtle.forward(30);
+        turtle.setPenWidth(3);
+        turtle.setPenColor(Color.GREEN);
+        turtle.turn(108);
+        turtle.forward(100);
+        turtle.turn(-250);
+        turtle.setPenWidth(9);
+        turtle.setPenColor(Color.RED);
+        turtle.forward(300);
+        turtle.turn(150);
+        turtle.setHeight(100);
+        turtle.setPenColor(Color.YELLOW);
+        turtle.forward(100);
     }
 }
