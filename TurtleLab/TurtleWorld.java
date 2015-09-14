49f3138441e586abe90877dfeecd94d0db661103
Ribrumfield <ribrumfield@NN123-HA-24.HS.sd203.org>
2015-09-14 13:47:28 -0500

    worked on turtle lab

--- TurtleLab/TurtleWorld.java
+++ TurtleLab/TurtleWorld.java
@@ -18,10 +18,21 @@ public class TurtleWorld
         turtle2.setPenColor(Color.GREEN);
         turtle2.forward(100);
         turtle.turn(90);
-        turtle.setPenWidth(5);
         turtle.forward(100);
         turtle2.turn(-90);
-        turtle2.setPenWidth(5);
         turtle2.forward(100);
+        turtle.turn(95);
+        turtle.forward(145);
+        turtle2.turn(-95);
+        turtle2.forward(155);
+        turtle.turn(110);
+        turtle.forward(95);
+        turtle2.turn(-120);
+        turtle2.forward(100);
+        turtle.turn(65);
+        turtle.forward(105);
+        turtle.turn(-150);
+        turtle.setPenColor(Color.BLUE);
+        turtle.forward(180);
     }
 }
