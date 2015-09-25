3da109046d627191bb7154f54d707e595e38bfa5
Ribrumfield <ribrumfield@NN123-HA-14.HS.sd203.org>
2015-09-25 14:13:55 -0500

    worked on chapter 4 labs with operators

---
+++ chapter 4 practice/DistanceConverter.java
@@ -0,0 +1,13 @@
+public class DistanceConverter
+{
+     public static final double yards = 3.5;
+     public static final double feet = yards * 3;
+     public static final double inches = feet * 12;
+   public static void main(String[] args)
+   {
+     
+      
+      System.out.println(yards + "yards are " + feet + "feet");
+      System.out.println(yards + "yards are " + inches + "inches");
+   }
+}
