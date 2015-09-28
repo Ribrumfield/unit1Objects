7d2bcc354da73d75cce199ac00ff14f2a3b2230a
Ribrumfield <ribrumfield@NN123-HA-14.HS.sd203.org>
2015-09-28 14:14:16 -0500

    finsished amnuity

--- chapter 4 practice/Annuity.java
+++ chapter 4 practice/Annuity.java
@@ -2,7 +2,15 @@ public class Annuity
 {
     public static void main (String []args)
     {
-        
+      double i = .08;
+      double n = 20;
+      double pvann = 10000;
+      double top = Math.pow ((1 + i),(n-1)) - 1;
+      double bot = Math.pow ((1 + i),(n - 1));
+      double frac = top / bot;
+      double secondtolast = frac + 1;
+      double finaL = secondtolast * pvann;
+      System.out.println(finaL);
     }
 }
     
