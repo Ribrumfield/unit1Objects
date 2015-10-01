b96218b798d0bac7d1e097f3fe6e41946f773824
Ribrumfield <ribrumfield@NN123-HA-14.HS.sd203.org>
2015-10-01 14:13:37 -0500

    string parsing with if else

--- chapter 4 practice/String_parsing.java
+++ chapter 4 practice/String_parsing.java
@@ -7,14 +7,26 @@ public class String_parsing
        Scanner s = new Scanner(System.in);
        String num;
        
-       System.out.print ("Please type an integer between 1,000 and 999,999");
+       System.out.print ("Please type an integer between 1,000 and 999,999 (please include the comma)");
        num = s.next ();
-       
-       String sub1 = num.substring(1,4);
-       String sub2 = num.substring(5,7);
+       int length = num.length();
+       if (length < 4)
+       {
+           System.out.println ("Your number was " + num);
+       }
+       else if (length = 4)
+       {
+           String sub1 = num.substring(0,3);
+           String sub2 = num.substring(0,3);
+       }
+           
+           
+       String sub1 = num.substring(0,3);
+       String sub2 = num.substring(4,7);
        
        String new_num = sub1 + sub2;
        
        System.out.println ("Your number was " + new_num);      
-    }
+      
+   }
 }
